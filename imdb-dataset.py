1dcfa843f51eeaf82eebc62e19d9ae0d6acc7c4a
anup-codes <anup2812007@gmail.com>
2026-07-14 20:37:45 +0530

    data cleaned

---
+++ imdb-dataset.py
@@ -0,0 +1,158 @@
+import numpy as np 
+import pandas as pd 
+
+df = pd.read_csv('IMDB Dataset.csv')
+
+
+df.duplicated().sum()
+df = df.drop_duplicates()
+
+# cleaning
+
+#lower case
+df['review'] = df['review'].str.lower()
+
+# remove whitespaces
+df['review'] = df['review'].str.strip()
+
+# remove html tags
+import re
+df['review'] = df['review'].str.replace(r'<.*?>', '', regex=True)
+
+# code to remove a URL
+df['review'] = df['review'].str.replace(r"https?://\S+|www\.\S+",'')
+
+# code to remove a URL
+df['review'] = df['review'].str.replace(
+    r'https?://\S+|www\.\S+',
+    '',
+    regex=True
+)
+
+# code to check if a review contains an URL
+df[df['review'].str.contains(r"https?://\S+|www\.\S+")]
+
+# expanding abbvr
+
+# expand 
+
+def remove_abb(data):
+    data = re.sub(r"he's", "he is", data)
+    data = re.sub(r"there's", "there is", data)
+    data = re.sub(r"We're", "We are", data)
+    data = re.sub(r"That's", "That is", data)
+    data = re.sub(r"won't", "will not", data)
+    data = re.sub(r"they're", "they are", data)
+    data = re.sub(r"Can't", "Cannot", data)
+    data = re.sub(r"wasn't", "was not", data)
+    data = re.sub(r"don\x89Ûªt", "do not", data)
+    data= re.sub(r"aren't", "are not", data)
+    data = re.sub(r"isn't", "is not", data)
+    data = re.sub(r"What's", "What is", data)
+    data = re.sub(r"haven't", "have not", data)
+    data = re.sub(r"hasn't", "has not", data)
+    data = re.sub(r"There's", "There is", data)
+    data = re.sub(r"He's", "He is", data)
+    data = re.sub(r"It's", "It is", data)
+    data = re.sub(r"You're", "You are", data)
+    data = re.sub(r"I'M", "I am", data)
+    data = re.sub(r"shouldn't", "should not", data)
+    data = re.sub(r"wouldn't", "would not", data)
+    data = re.sub(r"i'm", "I am", data)
+    data = re.sub(r"I\x89Ûªm", "I am", data)
+    data = re.sub(r"I'm", "I am", data)
+    data = re.sub(r"Isn't", "is not", data)
+    data = re.sub(r"Here's", "Here is", data)
+    data = re.sub(r"you've", "you have", data)
+    data = re.sub(r"you\x89Ûªve", "you have", data)
+    data = re.sub(r"we're", "we are", data)
+    data = re.sub(r"what's", "what is", data)
+    data = re.sub(r"couldn't", "could not", data)
+    data = re.sub(r"we've", "we have", data)
+    data = re.sub(r"it\x89Ûªs", "it is", data)
+    data = re.sub(r"doesn\x89Ûªt", "does not", data)
+    data = re.sub(r"It\x89Ûªs", "It is", data)
+    data = re.sub(r"Here\x89Ûªs", "Here is", data)
+    data = re.sub(r"who's", "who is", data)
+    data = re.sub(r"I\x89Ûªve", "I have", data)
+    data = re.sub(r"y'all", "you all", data)
+    data = re.sub(r"can\x89Ûªt", "cannot", data)
+    data = re.sub(r"would've", "would have", data)
+    data = re.sub(r"it'll", "it will", data)
+    data = re.sub(r"we'll", "we will", data)
+    data = re.sub(r"wouldn\x89Ûªt", "would not", data)
+    data = re.sub(r"We've", "We have", data)
+    data = re.sub(r"he'll", "he will", data)
+    data = re.sub(r"Y'all", "You all", data)
+    data = re.sub(r"Weren't", "Were not", data)
+    data = re.sub(r"Didn't", "Did not", data)
+    data = re.sub(r"they'll", "they will", data)
+    data = re.sub(r"they'd", "they would", data)
+    data = re.sub(r"DON'T", "DO NOT", data)
+    data = re.sub(r"That\x89Ûªs", "That is", data)
+    data = re.sub(r"they've", "they have", data)
+    data = re.sub(r"i'd", "I would", data)
+    data = re.sub(r"should've", "should have", data)
+    data = re.sub(r"You\x89Ûªre", "You are", data)
+    data = re.sub(r"where's", "where is", data)
+    data = re.sub(r"Don\x89Ûªt", "Do not", data)
+    data = re.sub(r"we'd", "we would", data)
+    data = re.sub(r"i'll", "I will", data)
+    data = re.sub(r"weren't", "were not", data)
+    data = re.sub(r"They're", "They are", data)
+    data = re.sub(r"Can\x89Ûªt", "Cannot", data)
+    data = re.sub(r"you\x89Ûªll", "you will", data)
+    data = re.sub(r"I\x89Ûªd", "I would", data)
+    data = re.sub(r"let's", "let us", data)
+    data = re.sub(r"it's", "it is", data)
+    data = re.sub(r"can't", "cannot", data)
+    data = re.sub(r"don't", "do not", data)
+    data = re.sub(r"you're", "you are", data)
+    data = re.sub(r"i've", "I have", data)
+    data = re.sub(r"that's", "that is", data)
+    data = re.sub(r"i'll", "I will", data)
+    data = re.sub(r"doesn't", "does not",data)
+    data = re.sub(r"i'd", "I would", data)
+    data = re.sub(r"didn't", "did not", data)
+    data = re.sub(r"ain't", "am not", data)
+    data = re.sub(r"you'll", "you will", data)
+    data = re.sub(r"I've", "I have", data)
+    data = re.sub(r"Don't", "do not", data)
+    data = re.sub(r"I'll", "I will", data)
+    data = re.sub(r"I'd", "I would", data)
+    data = re.sub(r"Let's", "Let us", data)
+    data = re.sub(r"you'd", "You would", data)
+    data = re.sub(r"It's", "It is", data)
+    data = re.sub(r"Ain't", "am not", data)
+    data = re.sub(r"Haven't", "Have not", data)
+    data = re.sub(r"Could've", "Could have", data)
+    data = re.sub(r"youve", "you have", data)  
+    data = re.sub(r"donå«t", "do not", data)
+    
+    return data
+
+df['review'] = df['review'].apply(remove_abb)
+
+# spelling correction using textblob
+
+from textblob import TextBlob
+
+def spelling_correction(text):
+    return TextBlob(text).correct().string
+
+df['review'].apply(spelling_correction)
+
+# punctuation
+import string
+string.punctuation
+
+def remove_puctuation(text):
+    
+    for i in string.punctuation:
+        if i in text:
+            text = text.replace(i,'')
+            
+    return text
+
+df['review'] = df['review'].apply(remove_puctuation)
+
